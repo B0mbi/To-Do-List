dbabb89e97c4292bbd8372db23eb90c2aca0f532
Paweł Tatys <pawel.tatys@cfi.pl>
2021-06-09 22:57:34 +0200

    Left Side Menu, Calendar Panel

--- To-Do-List/To-Do-List/MainForm.cs
+++ To-Do-List/To-Do-List/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,27 @@ using System.Windows.Forms;
 
 namespace To_Do_List
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            initComponent();
+        }
+
+        private void initComponent()
+        {
+            this.BackColor = Color.FromArgb(35, 47, 52);
+            leftSideMenuPanel.BackColor = Color.FromArgb(52, 73, 85);
+            foreach (Control item in leftSideMenuPanel.Controls)
+            {
+                if (item is Button)
+                {
+                    item.BackColor = Color.FromArgb(74, 101, 114);
+                }
+            }
+            tasksPanel.BackColor = Color.FromArgb(138, 176, 194);
+            calendarOperatorPanel.BackColor = Color.FromArgb(52, 73, 85);
         }
 
     }
