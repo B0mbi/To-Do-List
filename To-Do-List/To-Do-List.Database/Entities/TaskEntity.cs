7e5b497410a1d98578a2992905ee81e194f0bd11
Paweł Tatys <pawel.tatys@cfi.pl>
2021-06-10 09:36:38 +0200

    Task Entity Model

---
+++ To-Do-List/To-Do-List.Database/Entities/TaskEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List.Database.Entities
+{
+    class TaskEntity
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DayOfPerformance { get; set; }
+        public bool isDone { get; set; }
+    }
+}
